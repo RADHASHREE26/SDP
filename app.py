94f47934950cac877bb9903f10588aa27b1b8955
Radhashree Bose <radhag414@gmail.com>
2024-05-11 00:47:28 +0530

    Login and signup APIs checked

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+import pymssql
 from sqlalchemy import and_
 from flask import Flask, render_template, request, Response, jsonify
 from flask_restful import Api, Resource
@@ -10,7 +12,7 @@ from models import *
 
 app = Flask(__name__)
 
-db_string = "mssql+pymssql://"+ 'Bose01' + ":" + 'Bose01' + "@" + 'Radhashree' + "/" + 'farming_rental'
+db_string = "mssql+pymssql://Bose01:Bose01@Radhashree:1433/farming_rental"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 
 
@@ -28,21 +30,27 @@ logger.addHandler(sh)
 
 conn = sqlalchemy.create_engine(db_string)
 
+# conn = pymssql.connect(server='Radhashree', user='Bose01', password='Bose01', database='farming_rental')
+
 @app.route('/user_login', methods = ['GET', 'POST'])
 def user_login():
     if request.method == 'POST':
         data = json.loads(request.data)
-        user_id = data['user_id']
-        user_pw = data['user_pw']
-        db_entry = db.session.query(UserCredentials).filter(and_(UserCredentials.user_id == user_id, UserCredentials.user_pw == user_pw)).first()
+        var_user_id = data['user_id']
+        var_user_pw = data['user_pw']
+        print(data)
+        db_entry = db.session.query(UserCredentials).filter(and_(UserCredentials.user_id == var_user_id, UserCredentials.user_pw == var_user_pw)).first()
+        print(db_entry)
         if db_entry:
             return jsonify({'status':'valid user'})
         else:
             return jsonify({'status':'invalid user'})
+    return jsonify({"status":"ok"}), 200
         
 @app.route('/user_signup', methods = ['GET','POST'])
 def user_signup():
     if request.method == 'POST':
+        print(request.data)
         data = json.loads(request.data)
         username = data['username']
         address = data['address']
@@ -50,17 +58,34 @@ def user_signup():
         contact_number = data['contact_number']
         user_id = data['user_id']
         user_pw = data['user_pw']
+        created_on = datetime.now()
+
         db_entry = UserCredentials()
         db_entry.user_id = user_id
         db_entry.user_pw = user_pw
+        db_entry.created_on = created_on
         db.session.add(db_entry)
         db.session.commit()
 
-        db_entry = UserAccountDetails()
+        db_entry = UserDetails()
         db_entry.user_id = user_id
+        db_entry.username = username
         db_entry.contact_number = contact_number
         db_entry.contact_email = email
         db_entry.address = address
+        db_entry.created_on = created_on
+        db.session.add(db_entry)
+        db.session.commit()
+
+    return jsonify({"status": "created"}), 201
+
+
+@app.route('/account_update', methods = ['GET','POST'])
+def credentials_update():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        user_id = data['user_id']
+        db_entry = db.session.query()
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,13 +3,19 @@ from app import db
 class UserCredentials(db.Model):
     __tablename__ = 'user_credentials'
     __table_args__ = {"schema":"dbo"}
-    id = db.Column(db.Integer)
     user_id = db.Column(db.String(300), primary_key = True)
     user_pw = db.Column(db.String(300))
+    created_on = db.Column(db.DateTime)
+    updated_on = db.Column(db.DateTime)
 
-class UserAccountDetails(db.Model):
+class UserDetails(db.Model):
     __tablename__ = 'user_account_details'
     __table_args__ = {"schema":"dbo"}
     id = db.Column(db.Integer)
     user_id = db.Column(db.String(300), primary_key = True)
-    # username = db.Column(db.)
+    username = db.Column(db.String(300))
+    contact_number = db.Column(db.Integer)
+    contact_email = db.Column(db.String(300))
+    address = db.Column(db.String)
+    created_on = db.Column(db.DateTime)
+    updated_on = db.Column(db.Datetime)
