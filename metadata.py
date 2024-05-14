6426a2e5c35d20ac8891c7144fd6841927e17815
Radhashree Bose <Radhashree.Bose@chubb.com>
2024-05-14 16:24:42 +0530

    Modification

--- metadata.py
+++ metadata.py
@@ -6,7 +6,7 @@ registration_email_body = '''<html>
   You just registered with FarmEasy.
   We hope you enjoy your equipment-rental experiences with us.
 
-  <img src="templates/Farming_App.jpg" alt="Girl in a jacket" width="500" height="600">
+  <img src="templates/Farming_App.jpg" alt="farming image" width="500" height="600">
 
   To continue, click <a href="localhost:4200/">here</a>
 
@@ -22,7 +22,7 @@ account_update_email = '''<html>
   You account details for FarmEasy were just updated.
   We hope you are having a good experience with us.
 
-  <img src="templates/Farming_App.jpg" alt="Girl in a jacket" width="500" height="600">
+  <img src="templates/Farming_App.jpg" alt="farming image" width="500" height="600">
 
   If it wasn't you, please click <a href="localhost:4200/">here</a>.
 
